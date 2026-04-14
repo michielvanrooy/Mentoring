3a8a0e6d542883ecc0ee60eae409b79e64a94f6c
Michiel van Rooy <Michiel.vanRooy@standardbank.co.za>
2026-04-14 20:12:09 +0200

    got the solution working

--- Mentoring.Domain/Services/SomeOtherService.cs
+++ Mentoring.Domain/Services/SomeOtherService.cs
@@ -13,17 +13,15 @@ public class SomeOtherService : ISomeOtherService
 
     private string LetsTryAndSoSomethingFun(string theSomething)
     {
-        try
-        {
+        //try
+        //{
             throw new Exception("Something went horribly wrong and that is not fun");
-        }
-        catch (Exception ex)
-        {
-            Log.Error(ex, "Something broke");
+        //}
+        //catch (Exception ex)
+        //{
+        //    Log.Error(ex, "Something broke");
 
-            return string.Empty;
-
-            
-        }
+        //    return string.Empty;
+        //}
     }
 }
